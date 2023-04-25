153dd942ef063d5b406cb1e85d67c1206e096b7a
RoMartins <rodrigomartins_santos1804@hotmail.com>
2023-04-24 23:20:21 -0300

    config env.test and create test.sqlite

---
+++ src/test/transactions.test.ts
@@ -0,0 +1,52 @@
+import { beforeAll, afterAll, expect, it, describe, beforeEach } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+import { execSync } from 'node:child_process'
+
+describe('transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server).post('/transactions').send({
+      title: 'New transactions',
+      amount: 5000,
+      type: 'credit',
+    })
+    expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transactions',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transactions',
+        amount: 5000,
+      }),
+    ])
+  })
+})
